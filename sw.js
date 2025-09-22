fb0b41a4062e428a4f15679656c5956e8b0d9234
Aryan Mangla <aryanmangla05@icloud.com>
2025-09-22 19:15:48 +0530

    url chnaged

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for caching static assets
-const CACHE_NAME = 'sardar-fashion-v1';
+const CACHE_NAME = 'souka-stitches-v1';
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 
